d52abffc4907c23ae0ac5b52e54015deeffaf22b
K-Templar <david@jettisonnetworks.com>
2016-09-20 14:37:34 -0400

    Additional class definition. WIP

--- CrapsRoller/CrapsRoller/Form1.cs
+++ CrapsRoller/CrapsRoller/Form1.cs
@@ -10,16 +10,48 @@ using System.Windows.Forms;
 
 namespace CrapsRoller
 {
+    public class RollDice
+    {
+        private Random _rng;
+
+        public RollDice()
+        {
+            _rng = new Random();
+        }
+
+        // Properties
+        private uint _sided;
+        public uint HowMany { get; set; }
+        public uint Sided
+        {
+            get { return _sided; }
+            set { _sided = value; }
+        }
+
+        // Methods
+        private int InternalRoll(uint s)
+        {
+            return 1 + _rng.Next((int)s);
+        }
+
+        public int Roll()
+        {
+            return InternalRoll((uint)Sided);
+            //return (int)HowMany * (int)Sided;
+        }
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
         {
+            RollDice rolldice = new RollDice();
             InitializeComponent();
         }
 
         private void btnRollDice_Click(object sender, EventArgs e)
         {
-            RollDice.
+            
         }
     }
 }
