1953bbaf55707a12432e85cecb544ba47b2dc335
K-Templar <david@jettisonnetworks.com>
2016-09-21 11:10:03 -0400

    Initial working version - does rolling but no graphics

--- CrapsRoller/CrapsRoller/Form1.cs
+++ CrapsRoller/CrapsRoller/Form1.cs
@@ -10,6 +10,27 @@ using System.Windows.Forms;
 
 namespace CrapsRoller
 {
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        RollDice rolldice = new RollDice();
+
+        private void btnRollDice_Click(object sender, EventArgs e)
+        {
+            rolldice.HowMany = 2;
+            rolldice.Sided = 6;
+            //int result = rolldice.RollOne();
+            int[] myresult = rolldice.RollMany();
+            lblDie1.Text = myresult[0].ToString();
+            lblDie2.Text = myresult[1].ToString();
+        }
+    }
+
     public class RollDice
     {
         private Random _rng;
@@ -20,38 +41,28 @@ namespace CrapsRoller
         }
 
         // Properties
-        private uint _sided;
         public uint HowMany { get; set; }
-        public uint Sided
-        {
-            get { return _sided; }
-            set { _sided = value; }
-        }
-
+        public uint Sided { get; set; }
+        
         // Methods
         private int InternalRoll(uint s)
         {
             return 1 + _rng.Next((int)s);
         }
 
-        public int Roll()
+        public int RollOne()
         {
             return InternalRoll((uint)Sided);
-            //return (int)HowMany * (int)Sided;
-        }
-    }
-
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            RollDice rolldice = new RollDice();
-            InitializeComponent();
         }
 
-        private void btnRollDice_Click(object sender, EventArgs e)
+        public int[] RollMany()
         {
-            
+            int[] result = new int[HowMany];
+            for (int i = 0; i < HowMany; i++)
+            {
+                result[i] = InternalRoll(Sided);
+            }
+            return result;
         }
     }
 }
